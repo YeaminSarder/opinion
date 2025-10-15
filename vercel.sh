42b3f76f5ccac40dab2897a1e8a4ad741d7ea022
yeaminsarder <yeaminsardersr@gmail.com>
2025-10-15 16:17:07 +0600

    download rust

--- vercel.sh
+++ vercel.sh
@@ -1,6 +1,9 @@
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source $HOME/.cargo/env
+rustup target add wasm32-unknown-unknown
 cargo build --package opinion --target wasm32-unknown-unknown --release
 mkdir -p public
 cp target/wasm32-unknown-unknown/release/opinion.wasm public/opinion.wasm
-cp index.html public/index.html
-cp gl.js public/gl.js
+#cp index.html public/index.html
+#cp gl.js public/gl.js
 #cargo build --package opinion_api --bin api-hello --target "x86_64-unknown-linux-musl"
