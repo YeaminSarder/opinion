5fd0f7acede606781700d42b1a83609fa4a91b19
Omar <jtotheomer@gmail.com>
2025-10-17 13:14:05 +0600

    added Game logic

--- opinion/src/main.rs
+++ opinion/src/main.rs
@@ -1,9 +1,12 @@
 #![allow(dead_code)]
 
+use macroquad::prelude::coroutines::tweens::linear;
 //use macroquad::prelude::{camera::mouse};
-use macroquad::prelude as mcp;
+use macroquad::prelude::{self as mcp, debug};
 //use std::{fmt, hint::select_unpredictable};
-use std::fmt;
+use rand::seq::SliceRandom;
+use rand::{Rng, distr::Uniform, rng};
+use std::fmt; // for choose()
 
 //use macroquad::hash;
 //use macroquad::ui::root_ui;
@@ -22,7 +25,8 @@ pub fn new_uid() -> u32 {
 
 fn should_quit() -> bool {
     mcp::is_key_down(mcp::KeyCode::Q)
-        && (mcp::is_key_down(mcp::KeyCode::LeftControl) || mcp::is_key_down(mcp::KeyCode::RightControl))
+        && (mcp::is_key_down(mcp::KeyCode::LeftControl)
+            || mcp::is_key_down(mcp::KeyCode::RightControl))
 }
 
 fn draw_fps() {
@@ -34,6 +38,10 @@ pub trait RectExt {
     fn with_height(&mut self, w: f32) -> &mut mcp::Rect;
     fn with_width(&mut self, h: f32) -> &mut mcp::Rect;
     fn clip_by(&mut self, d: f32) -> &mut mcp::Rect;
+
+    fn update_pos_n_size(&mut self, x: f32, y: f32, w: f32, h: f32);
+
+    fn render(&self);
 }
 
 impl RectExt for mcp::Rect {
@@ -59,8 +67,18 @@ impl RectExt for mcp::Rect {
         self.h -= d * 2.0;
         self
     }
-}
 
+    fn update_pos_n_size(&mut self, x: f32, y: f32, w: f32, h: f32) {
+        self.x = x;
+        self.y = y;
+        self.w = w;
+        self.h = h;
+    }
+
+    fn render(&self) {
+        mcp::draw_rectangle(self.x, self.y, self.w, self.h, mcp::BROWN);
+    }
+}
 
 struct SizeRatio;
 impl SizeRatio {
@@ -80,9 +98,13 @@ impl SizeRatio {
     pub fn get_y(val: f32) -> f32 {
         mcp::screen_height() * val
     }
+
+    pub fn get(x: f32, y: f32) -> (f32, f32) {
+        (mcp::screen_width() * x, mcp::screen_height() * y)
+    }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum CardType {
     Attack,
     Defense,
@@ -90,6 +112,7 @@ pub enum CardType {
     Support,
 }
 
+#[derive(Clone)]
 pub struct CardImage {
     rows: u16,
     cols: u16,
@@ -191,6 +214,7 @@ fn resize_rect(rect: &mut mcp::Rect, edge: ResizeEdge, mouse_dx: f32, mouse_dy: 
     }
 }
 
+#[derive(Clone)]
 pub struct Card {
     id: u32,
     img: CardImage,
@@ -271,7 +295,13 @@ impl Shape {
     }
 }
 
-fn draw_text_in_rect_char_wrap(text: &str, rect: mcp::Rect, font: &mcp::Font, font_size: u16, color: mcp::Color) {
+fn draw_text_in_rect_char_wrap(
+    text: &str,
+    rect: mcp::Rect,
+    font: &mcp::Font,
+    font_size: u16,
+    color: mcp::Color,
+) {
     let scale = 1.0;
     let mut line = String::new();
     let mut y = rect.y + font_size as f32;
@@ -395,6 +425,50 @@ impl Renderer {
         Renderer::render_grid(0.0, 0.0, img.rows, img.cols, img.cell_size);
     }
 
+    fn render_card_minimal(card: &Card, font: &mcp::Font) {
+        let offset = 4.0;
+        let border = 4.0;
+        let font_size = card.rect.h / 6.0;
+        let dec_font_size = font_size / 1.5;
+
+        let mut img = card.rect.clone();
+        img.with_height(card.rect.h / 2.0).clip_by(border);
+
+        Shape::draw_rect(card.rect, mcp::DARKGRAY);
+        // Shape::draw_rect(img, BLUE);
+
+        // for c in card.name.chars() {
+        //     let dim = mcp::measure_text(&c.to_string(), Some(font), font_size as u16, 1.0);
+        //     println!("dim: {} {:?}", String::from(c), dim);
+        // }
+
+        let mut y = img.h + img.y + font_size;
+
+        mcp::draw_text_ex(
+            &card.name,
+            img.x,
+            y,
+            mcp::TextParams {
+                font: Some(&font),
+                font_size: font_size as u16,
+                color: mcp::WHITE,
+                ..Default::default()
+            },
+        );
+
+        y += dec_font_size + offset;
+
+        draw_text_in_rect_char_wrap(
+            &card.desc,
+            mcp::Rect::new(img.x, y, img.w, img.h),
+            font,
+            dec_font_size as u16,
+            mcp::WHITE,
+        );
+
+        // Renderer::render_card_img(&card.img);
+    }
+
     fn render_card(card: &Card, font: &mcp::Font) {
         let offset = 4.0;
         let border = 4.0;
@@ -470,6 +544,170 @@ fn window_conf() -> mcp::Conf {
     }
 }
 
+enum Tern {
+    Player1,
+    Player2,
+}
+
+pub struct Player {
+    hand: Vec<Card>,
+    library: Vec<Card>,
+    graveyard: Vec<Card>,
+
+    arena_rect: mcp::Rect,
+    hand_rect: mcp::Rect,
+    def_card_size: (f32, f32),
+}
+
+impl Player {
+    pub fn new(col: &CardCollection) -> Self {
+        let mut library = col.get_random(20);
+        for c in &library {
+            debug!("{}", c);
+        }
+
+        let hand = library.drain(library.len().saturating_sub(3)..).collect();
+
+        println!("---------------------------");
+
+        for c in &hand {
+            debug!("{}", c);
+        }
+
+        let arena_rect = SizeRatio::new(0.2, 0.52, 0.6, 0.18);
+        let hand_rect = SizeRatio::new(0.1, 0.78, 0.8, 0.18);
+        let def_card_size = (hand_rect.w / 7.0, hand_rect.h);
+
+        let mut ret = Self {
+            hand,
+            library,
+            graveyard: Vec::new(),
+            def_card_size,
+
+            arena_rect,
+            hand_rect,
+        };
+
+        ret.update_card_position();
+
+        ret
+    }
+
+    pub fn update_card_position(&mut self) {
+        let x = self.hand_rect.x;
+        let offset = 2.0;
+
+        for (ind, card) in self.hand.iter_mut().enumerate() {
+            let px = x + ind as f32 * (self.def_card_size.0 + offset);
+            card.rect.update_pos_n_size(
+                px,
+                self.hand_rect.y,
+                self.def_card_size.0,
+                self.def_card_size.1,
+            );
+        }
+    }
+
+    fn render_arena(&self, font: &mcp::Font) {
+        self.arena_rect.render();
+
+        let arena_text = "Arena";
+        let arena_text_width =
+            mcp::measure_text(arena_text, Some(font), self.arena_rect.h as u16, 1.);
+
+        mcp::draw_text_ex(
+            arena_text,
+            self.arena_rect.center().x - arena_text_width.width / 2.0,
+            self.arena_rect.bottom() - 20.0,
+            mcp::TextParams {
+                font: Some(&font),
+                font_size: self.arena_rect.h as u16,
+                color: mcp::WHITE,
+                ..Default::default()
+            },
+        );
+    }
+
+    pub fn render_hand(&self, font: &mcp::Font) {
+        self.hand_rect.render();
+        self.render_arena(font);
+
+        for card in self.hand.iter() {
+            Renderer::render_card_minimal(&card, font);
+        }
+    }
+}
+
+pub struct Game {
+    player1: Player,
+    player2: Player,
+    turn: Tern,
+
+    is_running: bool,
+    card_collection: Vec<Card>,
+}
+
+pub struct CardCollection {
+    col: Vec<Card>,
+}
+
+impl CardCollection {
+    pub fn generate() -> Self {
+        Self {
+            col: generate_card_collection(),
+        }
+    }
+
+    pub fn get_random(&self, count: usize) -> Vec<Card> {
+        rand::rng()
+            .sample_iter(Uniform::new_inclusive(0, self.col.len() - 1).unwrap())
+            .take(count)
+            .map(|ind| self.col[ind].clone())
+            .collect()
+    }
+}
+
+fn generate_card_collection() -> Vec<Card> {
+    let mut col = vec![];
+
+    let card_width = SizeRatio::get_x(0.4);
+    let card_height = SizeRatio::get_y(0.6);
+
+    let def_rect = mcp::Rect::new(0., 0., card_width, card_height);
+
+    let (img_rows, img_cols) = (10, 10);
+    let img = CardImage::new(img_rows, img_cols);
+
+    col.push(Card::new(
+        img.clone(),
+        "Smithy",
+        "Card draw 1",
+        0,
+        CardType::Support,
+        def_rect,
+    ));
+
+    col.push(Card::new(
+        img.clone(),
+        "Smithy2",
+        "Card draw 2",
+        0,
+        CardType::Support,
+        def_rect,
+    ));
+
+    col.push(Card::new(
+        img.clone(),
+        "Smithy3",
+        "Card draw 3",
+        0,
+        CardType::Support,
+        def_rect,
+    ));
+
+    col
+}
+
 #[cfg(not(target_arch = "wasm32"))]
 const FONT_PATH: &str = "public/assets/font/JetBrainsMono-Medium.ttf";
 
@@ -499,6 +737,10 @@ async fn main() {
 
     let mut cards = vec![fireball];
 
+    let mut card_collection = CardCollection::generate();
+
+    let mut player1 = Player::new(&mut card_collection);
+
     let font = mcp::load_ttf_font(FONT_PATH).await.unwrap();
 
     let mut mouse = Mouse::new();
@@ -516,13 +758,15 @@ async fn main() {
         //     .ui(&mut root_ui(), |ui| {});
 
         //update
-        for (ind, card) in cards.iter_mut().enumerate() {
-            card.update(&mut mouse, ind);
-        }
+        // for (ind, card) in cards.iter_mut().enumerate() {
+        //     card.update(&mut mouse, ind);
+        // }
+        //
+        // for card in cards.iter() {
+        //     Renderer::render_card(&card, &font);
+        // }
 
-        for card in cards.iter() {
-            Renderer::render_card(&card, &font);
-        }
+        player1.render_hand(&font);
 
         let mouse_contex = MouseContex {
             cards: Some(&mut cards),
