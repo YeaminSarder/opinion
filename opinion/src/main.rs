25fe74780e284ef5f064a023ba2a494ed2e10804
yeaminsarder <yeaminsardersr@gmail.com>
2025-10-24 11:50:42 +0600

    add prefix bp

--- opinion/src/main.rs
+++ opinion/src/main.rs
@@ -1,41 +1,41 @@
-use bevy::prelude::*;
+use bevy::prelude as bp;
 
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_systems(Startup, setup)
+    bp::App::new()
+        .add_plugins(bp::DefaultPlugins)
+        .add_systems(bp::Startup, setup)
         .run();
 }
 
 /// set up a simple 3D scene
 fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut commands: bp::Commands,
+    mut meshes: bp::ResMut<bp::Assets<bp::Mesh>>,
+    mut materials: bp::ResMut<bp::Assets<bp::StandardMaterial>>,
 ) {
     // circular base
     commands.spawn((
-        Mesh3d(meshes.add(Circle::new(4.0))),
-        MeshMaterial3d(materials.add(Color::WHITE)),
-        Transform::from_rotation(Quat::from_rotation_x(-std::f32::consts::FRAC_PI_2)),
+        bp::Mesh3d(meshes.add(bp::Circle::new(4.0))),
+        bp::MeshMaterial3d(materials.add(bp::Color::WHITE)),
+        bp::Transform::from_rotation(bp::Quat::from_rotation_x(-std::f32::consts::FRAC_PI_2)),
     ));
     // cube
     commands.spawn((
-        Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0))),
-        MeshMaterial3d(materials.add(Color::srgb_u8(124, 144, 255))),
-        Transform::from_xyz(0.0, 0.5, 0.0),
+        bp::Mesh3d(meshes.add(bp::Cuboid::new(1.0, 1.0, 1.0))),
+        bp::MeshMaterial3d(materials.add(bp::Color::srgb_u8(124, 144, 255))),
+        bp::Transform::from_xyz(0.0, 0.5, 0.0),
     ));
     // light
     commands.spawn((
-        PointLight {
+        bp::PointLight {
             shadows_enabled: true,
-            ..default()
+            ..bp::default()
         },
-        Transform::from_xyz(4.0, 8.0, 4.0),
+        bp::Transform::from_xyz(4.0, 8.0, 4.0),
     ));
     // camera
     commands.spawn((
-        Camera3d::default(),
-        Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
+        bp::Camera3d::default(),
+        bp::Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(bp::Vec3::ZERO, bp::Vec3::Y),
     ));
 }
