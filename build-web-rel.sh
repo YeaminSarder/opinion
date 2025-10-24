dea16482fee9ac1f855d6fe0a799c5a364c49442
yeaminsarder <yeaminsardersr@gmail.com>
2025-10-24 14:09:03 +0600

    optimization web using wasm-opt 7s -> 5s

--- build-web-rel.sh
+++ build-web-rel.sh
@@ -1,4 +1,5 @@
 cargo build --package opinion --target wasm32-unknown-unknown --profile wasm-release
 mkdir -p public
 cp target/wasm32-unknown-unknown/wasm-release/opinion.wasm public/opinion.wasm
+wasm-opt -O --output public/opinion.wasm public/opinion.wasm 
 wasm-bindgen --out-name  opinion --out-dir public --target web public/opinion.wasm
