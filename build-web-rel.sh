023d06589f1c8c43abeabbcfce052e7793e628bc
yeaminsarder <yeaminsardersr@gmail.com>
2025-10-24 13:53:24 +0600

    optimization web release build with size optimization 10s -> 7s

---
+++ build-web-rel.sh
@@ -0,0 +1,4 @@
+cargo build --package opinion --target wasm32-unknown-unknown --profile wasm-release
+mkdir -p public
+cp target/wasm32-unknown-unknown/wasm-release/opinion.wasm public/opinion.wasm
+wasm-bindgen --out-name  opinion --out-dir public --target web public/opinion.wasm
