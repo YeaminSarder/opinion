b791d787316ee6f2378e218153f46a90b5254288
yeaminsarder <yeaminsardersr@gmail.com>
2025-10-24 11:35:59 +0600

    bindgen

--- build-web.sh
+++ build-web.sh
@@ -1,3 +1,4 @@
 cargo build --package opinion --target wasm32-unknown-unknown
 mkdir -p public
 cp target/wasm32-unknown-unknown/debug/opinion.wasm public/opinion.wasm
+wasm-bindgen --out-name  opinion --out-dir public --target web public/opinion.wasm
